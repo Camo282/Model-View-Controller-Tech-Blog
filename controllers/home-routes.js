0fc5d03528f7a13ca5fb8f8a9348b418f80d63e8
Camo282 <camo282@gmail.com>
2022-10-18 13:19:37 -0400

    connect to jawsdb

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -11,7 +11,6 @@ router.get('/', (req, res) => {
       'post_content',
       'title',
       'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
     include: [
       {
